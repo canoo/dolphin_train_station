04dfeb6d86b2c25ac468614183380ad85dbe3f7b
Sven Ehrke <sven.ehrke@canoo.com>
2013-12-05 10:29:13 +0100

    launch scripts to start all clients

---
+++ startclients.sh
@@ -0,0 +1,4 @@
+client_fxboard/build/install/client_fxboard/bin/client_fxboard & 
+client_fxmap/build/install/client_fxmap/bin/client_fxmap & 
+client_swingboard/build/install/client_swingboard/bin/client_swingboard & 
+client_admin/build/install/client_admin/bin/client_admin & 
