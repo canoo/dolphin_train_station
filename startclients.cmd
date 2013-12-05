90ac6cb2ca6a71fda6bcbf7dddfb21e5b1b722a1
GirleaV <girlea.v@hotmail.com>
2013-12-05 10:39:41 +0100

    some changes to windows cmd start

--- startclients.cmd
+++ startclients.cmd
@@ -1,4 +1,4 @@
-client_fxboard\build\install\client_fxboard\bin\client_fxboard
-client_fxmap\build\install\client_fxmap\bin\client_fxmap
-client_swingboard\build\install\client_swingboard\bin\client_swingboard
-client_admin\build\install\client_admin\bin\client_admin
+start/B cmd /C call client_fxboard\build\install\client_fxboard\bin\client_fxboard.bat
+start/B cmd /C call client_fxmap\build\install\client_fxmap\bin\client_fxmap.bat
+start/B cmd /C call client_swingboard\build\install\client_swingboard\bin\client_swingboard.bat
+start/B cmd /C call client_admin\build\install\client_admin\bin\client_admin.bat
